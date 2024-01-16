653a290e62c341115a2eaa6793924c1a508ab787
ubergeekady <adysingh1989@gmail.com>
2024-01-16 22:08:33 +0530

    Initial Commit

---
+++ src/main/java/com/blackwood/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.blackwood.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerApplication.class, args);
+    }
+
+}
